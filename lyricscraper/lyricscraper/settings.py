3581932c226da7fbc998772ae12b349c64254df1
Thomas Bjerkan <thomas.bjerkan@student.manchester.ac.uk>
2017-06-12 16:34:04 +0100

    Initialise lyricscraper Scrapy project

---
+++ lyricscraper/lyricscraper/settings.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for lyricscraper project
+
+BOT_NAME = 'lyricscraper'
+
+SPIDER_MODULES = ['lyricscraper.spiders']
+NEWSPIDER_MODULE = 'lyricscraper.spiders'
+
+ROBOTSTXT_OBEY = True
