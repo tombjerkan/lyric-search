b1346a27d526eb294da36adeb344fc05854518a4
Thomas Bjerkan <thomas.bjerkan@student.manchester.ac.uk>
2017-06-12 17:10:36 +0100

    Extract song title and artist name from crawled pages

--- lyricscraper/lyricscraper/spiders/lyricspider.py
+++ lyricscraper/lyricscraper/spiders/lyricspider.py
@@ -18,14 +18,26 @@ class LyricSpider(scrapy.Spider):
             yield response.follow(next_page_link, callback=self.parse)
 
     def artist_parse(self, response):
+        artist_name = response.css('.artist-header h1 ::text').extract_first()
+
         song_selector = '#popular .songs-table a::attr(href)'
         song_links = response.css(song_selector).extract()
         for song_link in song_links:
-            yield response.follow(song_link, callback=self.song_parse)
+            yield response.follow(song_link, callback=self.song_parse,
+                                  meta={'artist': artist_name})
 
         next_page_link = response.css('.next::attr(href)').extract_first()
         if next_page_link:
             yield response.follow(next_page_link, callback=self.artist_parse)
 
     def song_parse(self, response):
-        pass
+        song_header_selector = '.lyrics header h1::text'
+        song_header = response.css(song_header_selector).extract_first().strip()
+
+        # Song header always of form "<song_name> Lyrics", so remove 'Lyrics'
+        song_name = song_header[:-7]
+
+        yield {
+            'artist': response.meta['artist'],
+            'song': song_name,
+        }
