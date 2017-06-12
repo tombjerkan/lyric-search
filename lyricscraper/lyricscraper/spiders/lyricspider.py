a2e26b02c1d84ebecee4c2c9999ff8d007cff8c3
Thomas Bjerkan <thomas.bjerkan@student.manchester.ac.uk>
2017-06-12 16:55:59 +0100

    Crawl artist song lists
    Follow links from artist lists to artists' song lists and crawl through the entire song list.

--- lyricscraper/lyricscraper/spiders/lyricspider.py
+++ lyricscraper/lyricscraper/spiders/lyricspider.py
@@ -9,6 +9,15 @@ class LyricSpider(scrapy.Spider):
                   for letter in string.ascii_lowercase]
 
     def parse(self, response):
+        artist_selector = '//table[@class="songs-table"]/tbody/tr/td/a/@href'
+        for artist_link in response.xpath(artist_selector):
+            yield response.follow(artist_link, callback=self.artist_parse)
+
         next_page_link = response.css('.next::attr(href)').extract_first()
         if next_page_link:
             yield response.follow(next_page_link, callback=self.parse)
+
+    def artist_parse(self, response):
+        next_page_link = response.css('.next::attr(href)').extract_first()
+        if next_page_link:
+            yield response.follow(next_page_link, callback=self.artist_parse)
