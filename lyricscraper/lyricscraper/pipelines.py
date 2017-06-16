e1a469616990744b22074fb4a2c0617ff8838527
Thomas Bjerkan <thomas.bjerkan@student.manchester.ac.uk>
2017-06-16 15:20:34 +0100

    Change order of conditions so that positive comes first

--- lyricscraper/lyricscraper/pipelines.py
+++ lyricscraper/lyricscraper/pipelines.py
@@ -4,15 +4,15 @@ from scrapy.exceptions import DropItem
 
 class LyricsPipeline:
     def process_item(self, item, spider):
-        if 'lyrics' not in item:
-            raise DropItem('Missing lyrics for {}'.format(item))
-        else:
+        if 'lyrics' in item:
             return item
+        else:
+            raise DropItem('Missing lyrics for {}'.format(item))
 
 
 class LanguagePipeline:
     def process_item(self, item, spider):
-        if langdetect.detect(item['lyrics']) != 'en':
-            raise DropItem('Lyrics for {} are not in English'.format(item))
-        else:
+        if langdetect.detect(item['lyrics']) == 'en':
             return item
+        else:
+            raise DropItem('Lyrics for {} are not in English'.format(item))
