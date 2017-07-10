3cfcb57d6b48d62cfdb4194c75246ef7ee492c7a
Thomas Bjerkan <thomas.bjerkan@student.manchester.ac.uk>
2017-07-10 13:40:31 +0100

    Create sparse vector representation of lyrics with counts of words occuring

--- lyric-search/lyricprocessor/documentvectors.py
+++ lyric-search/lyricprocessor/documentvectors.py
@@ -13,3 +13,15 @@ def create_dictionary(song_file):
         dictionary.add_documents([lyric_tokens])
 
     return dictionary
+
+
+def create_document_vectors(dictionary, song_file):
+    document_vectors = []
+
+    for song_json in song_file:
+        song = json.loads(song_json)
+        lyrics = song['lyrics']
+        lyric_tokens = nltk.word_tokenize(lyrics)
+        document_vectors.append(dictionary.doc2bow(lyric_tokens))
+
+    return document_vectors
