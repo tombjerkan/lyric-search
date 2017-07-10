3f7f8d90ee25f48ad3c6cebbfde95719a57781b1
Thomas Bjerkan <thomas.bjerkan@student.manchester.ac.uk>
2017-07-10 15:03:03 +0100

    Return bag-of-words representation of corpus from LyricCorpus class

--- lyric-search/lyricprocessor/documentvectors.py
+++ lyric-search/lyricprocessor/documentvectors.py
@@ -6,27 +6,22 @@ import nltk
 class LyricCorpus:
     def __init__(self, filename):
         self.filename = filename
+        self.dictionary = gensim.corpora.Dictionary()
 
     def __iter__(self):
         with open(self.filename) as song_file:
             for song_json in song_file:
                 song = json.loads(song_json)
-                yield nltk.word_tokenize(song['lyrics'])
+                lyric_tokens = nltk.word_tokenize(song['lyrics'])
+                yield self.dictionary.doc2bow(lyric_tokens, allow_update=True)
 
 
-def create_dictionary(lyric_corpus):
-    return gensim.corpora.Dictionary(lyric_corpus)
+def transform_document_vectors(lyric_corpus):
+    tfidf_model = gensim.models.TfidfModel(lyric_corpus)
+    tfidf_vectors = tfidf_model[lyric_corpus]
 
-
-def create_document_vectors(dictionary, lyric_corpus):
-    return [dictionary.doc2bow(lyric_tokens) for lyric_tokens in lyric_corpus]
-
-
-def transform_document_vectors(document_vectors, dictionary):
-    tfidf_model = gensim.models.TfidfModel(document_vectors)
-    tfidf_vectors = tfidf_model[document_vectors]
-
-    lsi_model = gensim.models.LsiModel(tfidf_vectors, id2word=dictionary)
+    lsi_model = gensim.models.LsiModel(tfidf_vectors,
+                                       id2word=lyric_corpus.dictionary)
     lsi_vectors = lsi_model[tfidf_vectors]
 
     return lsi_vectors
