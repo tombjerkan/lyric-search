3db5938b6141589c60d37c37bb4550b6de31bdfc
Thomas Bjerkan <thomas.bjerkan@student.manchester.ac.uk>
2017-07-18 16:22:59 +0100

    Save scraped song items to a database

--- lyric-search/lyricscraper/pipelines.py
+++ lyric-search/lyricscraper/pipelines.py
@@ -1,6 +1,9 @@
 import langdetect
 from scrapy.exceptions import DropItem
 
+import database.tools
+import database.models
+
 
 class LyricsPipeline:
     def process_item(self, item, spider):
@@ -16,3 +19,24 @@ class LanguagePipeline:
             return item
         else:
             raise DropItem('Lyrics for {} are not in English'.format(item))
+
+
+class DatabasePipeline:
+    def __init__(self, connection_string):
+        self.connection_string = connection_string
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(crawler.settings.get('DB_CONNECTION_STRING'))
+
+    def process_item(self, item, spider):
+        with database.tools.session_scope(self.connection_string) as session:
+            song = database.models.Song(
+                artist = item['artist'],
+                title = item['title'],
+                lyrics = item['lyrics']
+            )
+
+            session.add(song)
+
+        return item
