0669397239bd84ed277fe41c3867b6e54c382039
Thomas Bjerkan <thomas.bjerkan@student.manchester.ac.uk>
2017-07-18 16:35:14 +0100

    Add database table for vector features of songs

--- lyric-search/database/models.py
+++ lyric-search/database/models.py
@@ -12,3 +12,21 @@ class Song(Base):
     title = sqlalchemy.Column(sqlalchemy.String(100))
     artist = sqlalchemy.Column(sqlalchemy.String(100))
     lyrics = sqlalchemy.Column(sqlalchemy.String(2000))
+
+    vector = sqlalchemy.orm.relationship(
+        'VectorFeature',
+        order_by=lambda: VectorFeature.feature_index
+    )
+
+
+class VectorFeature(Base):
+    __tablename__ = 'VectorFeature'
+
+    song_id = sqlalchemy.Column(
+        sqlalchemy.Integer,
+        sqlalchemy.ForeignKey('Song.id'),
+        primary_key=True
+    )
+
+    feature_index = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    feature_value = sqlalchemy.Column(sqlalchemy.Float)
