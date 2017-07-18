ef577ce96c3350654ae347bdc9235d2cce7d4201
Thomas Bjerkan <thomas.bjerkan@student.manchester.ac.uk>
2017-07-18 12:50:27 +0100

    Add database table for songs

---
+++ lyric-search/database/models.py
@@ -0,0 +1,14 @@
+import sqlalchemy
+import sqlalchemy.ext.declarative
+
+
+Base = sqlalchemy.ext.declarative.declarative_base()
+
+
+class Song(Base):
+    __tablename__ = 'Song'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    title = sqlalchemy.Column(sqlalchemy.String(100))
+    artist = sqlalchemy.Column(sqlalchemy.String(100))
+    lyrics = sqlalchemy.Column(sqlalchemy.String(2000))
