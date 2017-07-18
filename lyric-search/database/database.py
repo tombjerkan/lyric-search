bbce4d3c23a6d24891fe31f256a855d0f66b99a5
Thomas Bjerkan <thomas.bjerkan@student.manchester.ac.uk>
2017-07-18 13:15:23 +0100

    Access database session through context manager

--- lyric-search/database/database.py
+++ lyric-search/database/database.py
@@ -1,3 +1,4 @@
+import contextlib
 import sqlalchemy
 
 from . import models
@@ -6,3 +7,19 @@ from . import models
 def create_database(database_file):
     engine = sqlalchemy.create_engine(database_file)
     models.Base.metadata.create_all(engine)
+
+
+@contextlib.contextmanager
+def session_scope(database_file):
+    engine = sqlalchemy.create_engine(database_file)
+    Session = sqlalchemy.orm.sessionmaker(bind=engine)
+    session = Session()
+
+    try:
+        yield session
+        session.commit()
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
