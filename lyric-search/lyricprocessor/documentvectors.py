55b2f3f12c49a2409829fea66e2619c4daa693dd
Thomas Bjerkan <thomas.bjerkan@student.manchester.ac.uk>
2017-07-19 21:48:29 +0100

    Use database for document vectors

--- lyric-search/lyricprocessor/documentvectors.py
+++ lyric-search/lyricprocessor/documentvectors.py
@@ -1,71 +1,57 @@
 #!/usr/bin/env python3
 
-import collections
 import gensim
-import json
 import nltk
 import sys
 
-
-SongVector = collections.namedtuple(
-    'SongVector',
-    ['artist', 'title', 'vector']
-)
+import database.tools
+import database.models
 
 
 class LyricCorpus:
-    def __init__(self, filename):
-        self.filename = filename
+    def __init__(self, connection_string):
+        self.connection_string = connection_string
         self.dictionary = gensim.corpora.Dictionary()
 
     def __iter__(self):
-        with open(self.filename) as song_file:
-            for song_json in song_file:
-                song = json.loads(song_json)
-                lyric_tokens = nltk.word_tokenize(song['lyrics'])
+        with database.tools.session_scope(self.connection_string) as session:
+            song_query = session.query(database.models.Song)
+            song_query = song_query.order_by(database.models.Song.id)
 
-                yield SongVector(
-                    song['artist'] if 'artist' in song else None,
-                    song['title'] if 'title' in song else None,
-                    self.dictionary.doc2bow(lyric_tokens, allow_update=True)
-                )
+            for song in song_query:
+                lyric_tokens = nltk.word_tokenize(song.lyrics)
+                yield self.dictionary.doc2bow(lyric_tokens, allow_update=True)
 
 
 def main():
-    lyrics_input_filename = sys.argv[1]
-    vectors_output_filename = sys.argv[2]
+    db_connection_string = sys.argv[1]
 
-    corpus = LyricCorpus(lyrics_input_filename)
+    corpus = LyricCorpus(db_connection_string)
     vectors = document_vectors(corpus)
-
-    with open(vectors_output_filename, 'w') as output_file:
-        save_vectors(vectors, output_file)
+    save_vectors(vectors, db_connection_string)
 
 
 def document_vectors(corpus):
-    tfidf = gensim.models.TfidfModel(song.vector for song in corpus)
+    tfidf = gensim.models.TfidfModel(corpus)
+    lsi = gensim.models.LsiModel(corpus, id2word=corpus.dictionary)
 
-    lsi = gensim.models.LsiModel(
-        (tfidf[song.vector] for song in corpus),
-        id2word=corpus.dictionary
-    )
-
-    for song in corpus:
-        lsi_vector = lsi[tfidf[song.vector]]
-        lsi_vector = [topic_value for _, topic_value in lsi_vector]
-        lsi_vector = tuple(lsi_vector)
-        yield SongVector(song.artist, song.title, lsi_vector)
+    for lyrics in corpus:
+        yield lsi[tfidf[lyrics]]
 
 
-def save_vectors(songs, file):
-    for song in songs:
-        song_json = json.dumps({
-            'artist': song.artist,
-            'title': song.title,
-            'vector': song.vector
-        })
+def save_vectors(vectors, connection_string):
+    with database.tools.session_scope(connection_string) as session:
+        for song_index, vector in enumerate(vectors, 1):
+            for feature in vector:
+                feature_index, feature_value = feature
 
-        file.write(song_json + '\n')
+                feature = database.models.VectorFeature(
+                    song_id=song_index,
+                    feature_index=int(feature_index),
+                    feature_value=float(feature_value)
+                )
+
+                session.add(feature)
 
 
 if __name__ == '__main__':
