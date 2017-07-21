2c0dff022a854dad2ed47384fee5b3332953c3ea
Thomas Bjerkan <thomas.bjerkan@student.manchester.ac.uk>
2017-07-21 14:05:42 +0100

    Stop saving vectors as they are not directly needed after index is generated

--- lyric-search/lyricprocessor/documentvectors.py
+++ lyric-search/lyricprocessor/documentvectors.py
@@ -71,7 +71,6 @@ def main():
     model = TfidfLsiModel.load(tfidf_filename, lsi_filename)
 
     vectors = model[corpus]
-    save_vectors(vectors, db_connection_string)
 
     index = gensim.similarities.Similarity(
         '/tmp/lyric-search.index',
@@ -79,21 +78,6 @@ def main():
         model.num_topics
     )
     index.save(index_filename)
-
-
-def save_vectors(vectors, connection_string):
-    with database.tools.session_scope(connection_string) as session:
-        for song_index, vector in enumerate(vectors, 1):
-            for feature in vector:
-                feature_index, feature_value = feature
-
-                feature = database.models.VectorFeature(
-                    song_id=song_index,
-                    index=int(feature_index),
-                    value=float(feature_value)
-                )
-
-                session.add(feature)
 
 
 if __name__ == '__main__':
