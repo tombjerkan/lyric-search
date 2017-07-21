8427ea7381b4dda2acb682503b2cc3d4f96b08fb
Thomas Bjerkan <thomas.bjerkan@student.manchester.ac.uk>
2017-07-21 13:37:05 +0100

    Add file IO methods for TfidfLsiModel

--- lyric-search/lyricprocessor/documentvectors.py
+++ lyric-search/lyricprocessor/documentvectors.py
@@ -33,9 +33,13 @@ class LyricCorpus:
 
 
 class TfidfLsiModel:
-    def __init__(self, corpus):
-        self.tfidf = gensim.models.TfidfModel(corpus)
-        self.lsi = gensim.models.LsiModel(corpus, id2word=corpus.dictionary)
+    def __init__(self, corpus=None):
+        if corpus is not None:
+            self.tfidf = gensim.models.TfidfModel(corpus)
+            self.lsi = gensim.models.LsiModel(
+                corpus,
+                id2word=corpus.dictionary
+            )
 
     def __getitem__(self, item):
         return self.lsi[self.tfidf[item]]
@@ -43,6 +47,17 @@ class TfidfLsiModel:
     @property
     def num_topics(self):
         return self.lsi.num_topics
+
+    def save(self, tfidf_filename, lsi_filename):
+        self.tfidf.save(tfidf_filename)
+        self.lsi.save(lsi_filename)
+
+    @classmethod
+    def load(cls, tfidf_filename, lsi_filename):
+        model = TfidfLsiModel()
+        model.tfidf = gensim.models.TfidfModel.load(tfidf_filename)
+        model.lsi = gensim.models.LsiModel.load(lsi_filename)
+        return model
 
 
 def main():
