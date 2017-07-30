84f17290c69cc3c9981e14f84b583cdb054f3e44
Thomas Bjerkan <thomas.bjerkan@student.manchester.ac.uk>
2017-07-30 18:48:19 +0100

    Documentation for pipelines module

--- lyric-search/lyricscraper/pipelines.py
+++ lyric-search/lyricscraper/pipelines.py
@@ -6,7 +6,14 @@ import database.models
 
 
 class LyricsPipeline:
+    """Ensures songs have lyrics.
+
+    Drops any SongItem that has not had lyrics assigned to it as songs without
+    lyrics are not useful to the application.
+    """
+
     def process_item(self, item, spider):
+        """Returns item if it has lyrics, drops it if not."""
         if 'lyrics' in item:
             return item
         else:
@@ -14,7 +21,19 @@ class LyricsPipeline:
 
 
 class LanguagePipeline:
+    """Ensures song's lyrics are in English.
+
+    Drops any SongItem whose lyrics are not in English as only these songs are
+    wanted for the application.
+    """
+
     def process_item(self, item, spider):
+        """Returns item if its lyrics are in English, drops it if not.
+
+        Language detection is not always correct so it is possible some songs
+        with English lyrics are dropped and some songs with lyrics in other
+        languages are not.
+        """
         if langdetect.detect(item['lyrics']) == 'en':
             return item
         else:
@@ -22,7 +41,10 @@ class LanguagePipeline:
 
 
 class DatabasePipeline:
+    """Inserts each song's information into the database."""
+
     def process_item(self, item, spider):
+        """Inserts a song into the database using fields from SongItem."""
         with database.tools.session_scope() as session:
             song = database.models.Song(
                 artist = item['artist'],
