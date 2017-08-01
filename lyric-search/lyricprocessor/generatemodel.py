6fd0a5e5a391d0e0f46e706f5a274b9851cceb60
Thomas Bjerkan <thomas.bjerkan@student.manchester.ac.uk>
2017-08-02 00:39:21 +0100

    Documentation for generatemodel script

--- lyric-search/lyricprocessor/generatemodel.py
+++ lyric-search/lyricprocessor/generatemodel.py
@@ -1,3 +1,10 @@
+"""Generates the TF-IDF/LSI model for the song lyrics.
+
+Generates the TfidfLsiModel that transforms lyric documents into vector
+representations. The lyrics are taken from the database and saved to the two
+model files, both specified in the config file.
+"""
+
 from lyricprocessor import LyricCorpus
 from lyricprocessor import TfidfLsiModel
 
