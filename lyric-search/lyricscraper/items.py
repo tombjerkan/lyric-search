44eee136e2c24d54d90ddbfa474a0cb9d2028a6e
Thomas Bjerkan <thomas.bjerkan@student.manchester.ac.uk>
2017-08-02 00:42:08 +0100

    Documentation for items module

--- lyric-search/lyricscraper/items.py
+++ lyric-search/lyricscraper/items.py
@@ -3,12 +3,20 @@ from scrapy.loader.processors import MapCompose, TakeFirst, Join
 
 
 class SongItem(scrapy.Item):
+    """Contains data for scraped songs."""
+
     artist = scrapy.Field()
     title = scrapy.Field()
     lyrics = scrapy.Field()
 
 
 def remove_lyrics_ending(title):
+    """Removes the suffix from the title of lyrics.
+
+    Song titles on MetroLyrics are of the form 'X Lyrics', where X is the name
+    of the song. To get just the song name, the ' Lyrics' suffix must be
+    removed.
+    """
     if title[-len(' Lyrics'):] == ' Lyrics':
         return title[:-len(' Lyrics')]
     else:
@@ -16,6 +24,12 @@ def remove_lyrics_ending(title):
 
 
 class SongItemLoader(scrapy.loader.ItemLoader):
+    """Loads fields for SongItem objects.
+
+    The fields are processed when being assigned to a SongItem object and when
+    being used.
+    """
+
     artist_in = MapCompose(lambda s: s.strip())
     artist_out = TakeFirst()
 
