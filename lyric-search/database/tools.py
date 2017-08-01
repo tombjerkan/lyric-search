f8428572480a5efeeb0936dbfa6189a5b42b8a3b
Thomas Bjerkan <thomas.bjerkan@student.manchester.ac.uk>
2017-08-02 00:36:59 +0100

    Documentation for database.tools module

--- lyric-search/database/tools.py
+++ lyric-search/database/tools.py
@@ -8,12 +8,25 @@ _config = ConfigObj('settings.cfg')
 
 
 def create_database():
+    """Creates the database and its tables.
+
+    The database is created according to the DB_CONNECTION_STRING config
+    setting.
+    """
     engine = sqlalchemy.create_engine(_config['DB_CONNECTION_STRING'])
     models.Base.metadata.create_all(engine)
 
 
 @contextlib.contextmanager
 def session_scope():
+    """Returns a session context for the database.
+
+    Returns a session context for the database given by the
+    DB_CONNECTION_STRING config setting.
+
+    Committing, rollbacks and closing for the session is handled automatically
+    by the context manager.
+    """
     engine = sqlalchemy.create_engine(_config['DB_CONNECTION_STRING'])
     Session = sqlalchemy.orm.sessionmaker(bind=engine)
     session = Session()
