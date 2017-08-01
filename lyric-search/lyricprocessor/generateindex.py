85e00ab88efeee522eb796fe43ba8e8d18c44cb9
Thomas Bjerkan <thomas.bjerkan@student.manchester.ac.uk>
2017-08-02 00:38:46 +0100

    Documentation for generateindex script

--- lyric-search/lyricprocessor/generateindex.py
+++ lyric-search/lyricprocessor/generateindex.py
@@ -1,3 +1,11 @@
+"""Generates the similarity index for the song lyrics.
+
+Generates a Gensim similarity index to be used when querying lyric similarity.
+The lyrics are taken from the database, the TF-IDF/LSI model is loaded from
+previously generated files and the resulting index is saved to the index file,
+all specified in the config file.
+"""
+
 import gensim
 
 from lyricprocessor import LyricCorpus
