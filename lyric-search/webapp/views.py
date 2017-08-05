54044d2fa8bc132f6dbfa51662b9db89da5210a4
Thomas Bjerkan <thomas.bjerkan@student.manchester.ac.uk>
2017-08-05 14:14:49 +0100

    Create query similarity page

--- lyric-search/webapp/views.py
+++ lyric-search/webapp/views.py
@@ -7,3 +7,8 @@ from webapp import app
 @app.route('/index')
 def index():
     return flask.render_template('index.html')
+
+
+@app.route('/querysimilarity', methods=['GET', 'POST'])
+def query():
+    return flask.render_template('querysimilarity.html')
